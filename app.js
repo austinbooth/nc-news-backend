e432677b5b8c8813c35ab530c1e13d3067206d4c
austinbooth <austinbooth78@gmail.com>
2020-08-05 23:02:44 +0100

    Refactored

--- app.js
+++ app.js
@@ -17,15 +17,12 @@ function handlePSQL400Errors(err, req, res, next) {
 }
 
 function handlePSQL404Errors(err, req, res, next) {
-  if (err.code === "23503" && err.reason === "- username not given")
-    res.status(400).send({ msg: `Invalid ${err.reason}` });
-  else if (err.code === "23503")
+  if (err.code === "23503")
     res.status(404).send({ msg: `Invalid ${err.reason}` });
   else next(err);
 }
 
 function handleCustomErrors(err, req, res, next) {
-  // console.log(err, "<<<<<");
   res.status(err.status).send({ msg: err.msg });
 }
 
