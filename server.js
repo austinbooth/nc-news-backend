5dc79bf90de2a305a2e317042c96f120715d02cd
austinbooth <austinbooth78@gmail.com>
2020-08-27 13:15:51 +0100

    Corrected router error

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { PORT = 9090 } = process.env;
 const express = require("express");
 
-const apiRouter = require("./routers/routers");
+const apiRouter = require("./routers/api.router");
 
 const app = express();
 app.use("/api", apiRouter);
