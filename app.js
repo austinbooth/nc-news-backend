6b0358bfad2b0850928d4c10bb233ec5198fe6f6
austinbooth <austinbooth78@gmail.com>
2020-08-28 13:40:27 +0100

    Added CORS to allow frontend to use API

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const {
   handlePSQL400Errors,
   handlePSQL404Errors,
 } = require("./errors");
+const cors = require("cors");
 
 const app = express();
+app.use(cors);
 app.use(express.json());
 
 app.use("/api", apiRouter);
