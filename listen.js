cf00fbc7bfa3f98ffba1d0d4f63d77b430af89cd
austinbooth <austinbooth78@gmail.com>
2020-08-28 14:05:52 +0100

    Added cors

--- listen.js
+++ listen.js
@@ -1,9 +1,11 @@
 const { PORT = 9090 } = process.env;
 const express = require("express");
+const cors = require("cors");
 
 const apiRouter = require("./routers/api.router");
 
 const app = express();
+app.use(cors());
 app.use("/api", apiRouter);
 
 app.use(express.json());
