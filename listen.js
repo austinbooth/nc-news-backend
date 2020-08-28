2a1ad7ab71d8ba618510734b03e5d47f93c71599
austinbooth <austinbooth78@gmail.com>
2020-08-28 14:21:12 +0100

    Tidied up

--- listen.js
+++ listen.js
@@ -1,15 +1,4 @@
 const { PORT = 9090 } = process.env;
-const express = require("express");
-const cors = require("cors");
-
-const apiRouter = require("./routers/api.router");
-
-const app = express();
-app.use(cors());
-app.use("/api", apiRouter);
-
-app.use(express.json());
+const app = require("./app");
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
-
-module.exports = app;
